d218fb3f941736ef8dc52c339a532ea67b57c7ab
Juan Ferraro <juansebastianferraro@gmail.com>
2023-10-06 12:07:46 -0500

    Tasks -> Added delete task.

--- tasks/views.py
+++ tasks/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Task
 
 # 
-def list_tasks(request):
-    
+def list_tasks(request):   
     tasks = Task.objects.all()
     print(tasks)
     return render(request, 'list_tasks.html/', {'tasks': tasks})
@@ -19,4 +18,20 @@ def create_task(request):
     
     task = Task(title=request.POST['title'], description=request.POST['description'])
     task.save()
+    return redirect('/tasks/')
+
+#Delete task
+def delete_task(request, task_id):
+    """_DELETE TASK_
+
+    Args:
+        request: _Request_
+        task_id (_id_): _This is the task's ID to delete_
+
+    Returns:
+        _Redirect to the main and only page_
+    """
+
+    delete_task = Task.objects.get(id=task_id)
+    delete_task.delete()
     return redirect('/tasks/')
