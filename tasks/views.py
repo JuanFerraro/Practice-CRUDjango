a0e818d766bd018b2e41a073562e2c455ad48617
Juan Ferraro <juansebastianferraro@gmail.com>
2023-10-06 09:17:44 -0500

    Tasks -> Create task ✅

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
 
 # 
 def list_tasks(request):
     return render(request, 'list_tasks.html/')
+
+
+#Create task
+def create_task(request):
+    """_CREATE TASK_
+    This Function create a Task 
+    Args:
+        request: _Request_
+    """
+    
+    task = Task(title=request.POST['title'], description=request.POST['description'])
+    task.save()
+    return redirect('/tasks/')
